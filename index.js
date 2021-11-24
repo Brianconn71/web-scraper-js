e4d0ea2eb3749f307e8c73831a62eacdd996e606
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-24 15:26:36 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const PORT = 8000;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express()
+
+const url = 'https://www.theguardian.com/uk'
+
+axios(url)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`) )
